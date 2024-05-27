1f6626a1eb61c27062819158ac00c50380d585cc
Navid Hosseini <hosseini.zari@gmail.com>
2024-05-27 15:12:26 +0200

    Init add some component

---
+++ src/components/Button.stories.tsx
@@ -0,0 +1,9 @@
+
+import Button from './Button';
+
+export default {
+    title: 'Coponents/Button',
+    component: Button,
+};
+
+export const Primary = () => <Button>Primary Button</Button>;
---
+++ src/components/Button.tsx
@@ -0,0 +1,17 @@
+import styled from 'styled-components';
+
+const Button = styled.button`
+  background: palevioletred;
+  border-radius: 3px;
+  border: none;
+  color: white;
+  padding: 0.5em 1em;
+  font-size: 1em;
+  cursor: pointer;
+
+  &:hover {
+    background: #d45d7d;
+  }
+`;
+
+export default Button;
